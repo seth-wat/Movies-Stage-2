d7a226a066f2fd4eb988ad27e5c19edc2fa63b9d
Seth Watkins <seth.personal.smtp@gmail.com>
2017-08-27 10:56:34 -0500

    Bind views and populate some data for test purposes.

--- app/src/main/java/com/example/android/popmovies/DetailActivity.java
+++ app/src/main/java/com/example/android/popmovies/DetailActivity.java
@@ -1,45 +1,61 @@
 package com.example.android.popmovies;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.popmovies.data.Movie;
+import com.example.android.popmovies.data.Review;
+import com.example.android.popmovies.databinding.ActivityDetailBinding;
 import com.example.android.popmovies.loaders.DetailLoader;
+import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import static android.view.MotionEvent.ACTION_SCROLL;
 
 public class DetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Movie> {
 
     Movie myMovie;
+    ActivityDetailBinding mBinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        TextView mTitleTextView = (TextView) findViewById(R.id.title_text_view);
-        ImageView mImageView = (ImageView) findViewById(R.id.detail_image_view);
-        TextView mReleaseDateTextView = (TextView) findViewById(R.id.release_date_text_view);
-        TextView mUserRatingTextView = (TextView) findViewById(R.id.user_rating_text_view);
-        TextView mSynopsisTextView = (TextView) findViewById(R.id.synopsis_text_view);
+        mBinder = DataBindingUtil.setContentView(this, R.layout.activity_detail);
 
         Intent mIntent = getIntent();
         myMovie = Parcels.unwrap(mIntent.getParcelableExtra("movieParcel"));
         if (myMovie != null) {
             Toast.makeText(this, "The movie was passed and unwrapped successfully!", Toast.LENGTH_LONG).show();
         }
+        mBinder.titleTextView.setText(myMovie.getTitle());
+        mBinder.releaseDateTextView.setText(myMovie.getReleaseDate());
+        mBinder.ratingTextView.setText(myMovie.getUserRating() + " / 10");
+        mBinder.durationTextView.setText("120 min");
 
+        mBinder.synopsisTextView.setText(myMovie.getPlotSynopsis());
+        Picasso.with(this).load(myMovie.getDetailImagePath()).into(mBinder.dropImageView);
         getSupportLoaderManager().initLoader(123, null, this);
 
 
 
+
     }
+
+
 
 
     @Override
@@ -50,12 +66,15 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
     @Override
     public void onLoadFinished(Loader<Movie> loader, Movie data) {
         if (data != null) {
-            if (!data.getReviews().isEmpty()) {
-                String author = data.getReviews().get(0).getAuthor();
-                Toast.makeText(this, author, Toast.LENGTH_LONG).show();
+            final ArrayList<Review> reviews = myMovie.getReviews();
+            int currentIndex = 0;
+            if (!(reviews.size() < 0)) {
+                mBinder.reviewInclude.userNameTextView.setText(reviews.get(0).getAuthor());
+                mBinder.reviewInclude.synopsisFrame.setText(reviews.get(0).getContent());
             }
+            
             if (!data.getVideos().isEmpty()) {
-
+                return;
             }
         }
     }
