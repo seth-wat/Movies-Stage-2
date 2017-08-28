4ab6dc8fc40b04fa2f80548b9406b64ebaee77cd
Seth Watkins <seth.personal.smtp@gmail.com>
2017-08-28 10:11:45 -0500

    Adapter to load reviews.

--- app/src/main/java/com/example/android/popmovies/ReviewAdapter.java
+++ app/src/main/java/com/example/android/popmovies/ReviewAdapter.java
@@ -1,11 +1,17 @@
 package com.example.android.popmovies;
 
+import android.app.Activity;
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.android.popmovies.data.Review;
+import com.example.android.popmovies.databinding.ActivityDetailBinding;
+import com.example.android.popmovies.databinding.LayoutReviewBinding;
 
 import java.util.ArrayList;
 
@@ -13,30 +19,41 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ViewHolder
     private LayoutInflater mInflater;
     private ArrayList<Review> reviews;
     private Context context;
+    private Activity activity;
 
-    public ReviewAdapter(Context context, ArrayList<Review> reviews) {
+    public ReviewAdapter(Context context, Activity activity, ArrayList<Review> reviews) {
         mInflater = LayoutInflater.from(context);
         this.context = context;
+        this.activity = activity;
         this.reviews = reviews;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = mInflater.inflate(R.layout.layout_review, parent, false);
+        return new ViewHolder(view);
+
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        Review review = reviews.get(position);
+        holder.author.setText(review.getAuthor());
+        holder.content.setText(review.getContent());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return reviews.size();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder() {
+        TextView author;
+        TextView content;
+        public ViewHolder(View view) {
+            super(view);
+            author = (TextView) view.findViewById(R.id.user_name_text_view);
+            content = (TextView) view.findViewById(R.id.synopsis_frame);
 
         }
 
