4f66680981e76cd03555f640f5511f527e58e81f
Seth Watkins <seth.personal.smtp@gmail.com>
2017-08-30 07:57:16 -0500

    Attach onClickListener.

--- app/src/main/java/com/example/android/popmovies/TrailerAdapter.java
+++ app/src/main/java/com/example/android/popmovies/TrailerAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.popmovies;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,6 +9,9 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.android.popmovies.data.Video;
+import com.example.android.popmovies.databinding.ActivityDetailBinding;
+import com.example.android.popmovies.events.TrailerClickHandler;
+import com.example.android.popmovies.utilities.NetworkUtils;
 
 import org.w3c.dom.Text;
 
@@ -20,10 +24,12 @@ import java.util.ArrayList;
 public class TrailerAdapter extends RecyclerView.Adapter<TrailerAdapter.ViewHolder>{
     private ArrayList<Video> videos;
     private LayoutInflater layoutInflater;
+    private Activity activity;
 
-    public TrailerAdapter(Context context, ArrayList<Video> videos) {
+    public TrailerAdapter(Context context, ArrayList<Video> videos, Activity activity) {
         this.videos = videos;
         this.layoutInflater = LayoutInflater.from(context);
+        this.activity = activity;
     }
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -34,6 +40,7 @@ public class TrailerAdapter extends RecyclerView.Adapter<TrailerAdapter.ViewHold
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.name.setText(videos.get(position).getName());
+        holder.name.setOnClickListener(new TrailerClickHandler(NetworkUtils.YOUTUBE_BASE_URL + videos.get(position).getKey(), activity));
     }
 
     @Override
